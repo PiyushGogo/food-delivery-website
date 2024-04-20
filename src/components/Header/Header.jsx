284d35f213680c434737e5b74728742ea26a9c4f
Piyush cn <piyush.gogoriya@capitalnow.in>
2024-04-21 01:02:39 +0530

    Explore menu page

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your favorite Food</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          illum hic facilis dolorum! Expedita laborum atque aperiam sequi ipsam
+          numquam nesciunt, dolores veritatis porro eveniet, molestias impedit
+          nam odit molestiae!
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
